550c3ab28f932737b6707d0ecb14006b69af170d
rajafulara <rajafulara.rf@gmail.com>
2025-01-11 17:52:26 +0530

    fixed ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,29 +4,42 @@ import axios from 'axios'
 
 function App() {
 
-  const [jokes, setJokes]= useState([])
+  const [jokes, setJokes] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
 
-  useEffect(()=>{
-    axios.get(process.env.BACKEND_URL+'/api/jokes').then((res)=>{
+  useEffect(() => {
+    axios.get(process.env.BACKEND_URL + '/api/jokes').then((res) => {
       setJokes(res.data)
+      setLoading(false)
     })
-    .catch((err)=>{
-      console.log(err)
-    })
-  },[])
+      .catch((err) => {
+        console.log(err)
+        setError(true)
+      })
+  }, [])
 
   return (
     <>
-      <h1>Hello World</h1>
-      <p>JOKES: {jokes.length}</p>
-      {
-        jokes.map((joke,index)=>(
-          <div key={joke.id}>
-            <h3>{joke.title}</h3>
-            <p>{joke.content}</p>
-          </div>
-        ))
-      }
+      <h1>Hello from frontend</h1>
+      {loading ? (
+        <div>
+          {!error ? (<p>Loading backend...</p>):(<p>Something went wrong</p>)}
+        </div>
+      ) : (
+        <div>
+          <h3>Jokes from backend</h3>
+          <p>Total jokes: {jokes.length}</p>
+          {
+            jokes.map((joke, index) => (
+              <div key={joke.id}>
+                <h3>{joke.title}</h3>
+                <p>{joke.content}</p>
+              </div>
+            ))
+          }
+        </div>
+      )}
     </>
   )
 }
